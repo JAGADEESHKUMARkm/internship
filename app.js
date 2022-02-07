a5450ec095e5305b7f47e88a3f3f8001d4b4cd83
JAGADEESHKUMARkm <98955310+JAGADEESHKUMARkm@users.noreply.github.com>
2022-02-07 15:21:40 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,125 @@
+const express = require ('express')
+const mongoose = require('mongoose')
+const Student = require('./studentModel.js')
+const Standard = require('./standardModel.js')
+
+const url = 'mongodb://127.0.0.1:27017/studentdatabase'
+
+const app = express();
+
+mongoose.connect(url);
+mongoose.connection.once('open',function(){
+    console.log('connected to the Mongodb');
+})
+app.use(express.json())
+app.get('/getStudent/:classId', async(req,res) => {
+    try {
+        const students = await Student.find()
+        const studentGroup = []
+        students.map(student => {
+            if(student.classId == req.params.classId)
+            {
+                studentGroup.push(student)
+            }
+        })  
+        res.json(studentGroup)
+    } catch (err) {
+        res.send('Error ' + err)
+    }
+})
+
+app.get('/getStudents', async(req,res) => {
+    try {
+        const students = await Student.find()
+        res.json(students)
+    } catch (err) {
+        res.send('Error ' + err)
+    }
+})
+
+app.post('/addStudents',async(req,res) => {
+    const students = new Student({
+        name : req.body.name,
+        rollno : req.body.rollno,
+        mobile : req.body.mobile,
+        classId : req.body.classId
+    })
+
+    try {
+        const data = await students.save()
+        res.json(data)
+    } catch (err) {
+        res.send('Error')
+    }
+}) 
+
+app.put('/updateStudents/:id', async(req,res) => {
+    try {
+        const student = await Student.findById(req.params.id)
+        student.name = req.body.name
+        student.rollno = req.body.rollno
+        student.mobile = req.body.mobile,
+        student.classId = req.body.classId 
+        const data = await student.save()
+        res.json(data)
+    } catch (err) {
+        res.send('Error')
+    }
+})
+
+app.delete('/deleteStudents/:id', async(req,res) => {
+    try {
+        const student = await Student.findById(req.params.id) 
+        const data = await student.remove()
+        res.json(data)
+    } catch (err) {
+        res.send('Error')
+    }
+})
+
+/*Class*/
+
+app.get('/getStandard', async(req,res) => {
+    try {
+        const standard = await Standard.find()
+        res.json(standard)
+    } catch (err) {
+        res.send('Error ' + err)
+    }
+})
+
+app.post('/addStandard',async(req,res) => {
+    const standard = new Standard ({
+        std : req.body.std,
+        division : req.body.division
+    })
+    try {
+        const data = await standard.save()
+        res.json(data)
+    } catch (err) {
+        res.send('Error')
+    }
+}) 
+
+app.put('/updateStandard/:id', async(req,res) => {
+    try {
+        const standard = await Standard.findById(req.params.id)
+        standard.std = req.body.std
+        standard.division = req.body.division 
+        const data = await standard.save()
+        res.json(data)
+    } catch (err) {
+        res.send('Error')
+    }
+})
+
+app.delete('/deleteStandard/:id', async(req,res) => {
+    try {
+        const standard = await Standard.findById(req.params.id) 
+        const data = await standard.remove()
+        res.json(data)
+    } catch (err) {
+        res.send('Error')
+    }
+})
+app.listen(4000, () => {console.log('Server Started')})
---
+++ studentModel.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const studentSchema = new mongoose.Schema({
+    name: { type: String, required: true},
+    rollno: { type: Number, required: true},
+    mobile: { type: Number, required: true},
+    classId: { type: String, required: true}
+});
+
+module.exports = mongoose.model('Student', studentSchema);
